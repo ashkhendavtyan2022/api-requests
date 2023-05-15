ad7b887fd6e5e86903c2944f65e4fbeb982cb0bc
ashkhendavtyan2022 <119871908+ashkhendavtyan2022@users.noreply.github.com>
2023-05-15 23:39:17 +0400

    Latest Version

--- react-app/src/Platform/Api/index.jsx
+++ react-app/src/Platform/Api/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const Api = "https://crudcrud.com/api/f387e721807144bdbc6805ed70d4f562/"
+const Api = "https://crudcrud.com/api/1009d5bb1a14442dbfc39c5543ae79b2"
 
 export const SetUser = (data) => {
     return axios.post(`${Api}list`, data)
